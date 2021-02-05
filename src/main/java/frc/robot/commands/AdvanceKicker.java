a97b1dfe90f7e32e2fee74000f47c15506527d53
Micah Guttman <memtech3@gmail.com>
2021-02-04 21:38:50 -0600

    made the kicker stop when ended

--- src/main/java/frc/robot/commands/AdvanceKicker.java
+++ src/main/java/frc/robot/commands/AdvanceKicker.java
@@ -37,7 +37,9 @@ public class AdvanceKicker extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    kicker.stopKicker();
+  }
 
   // Returns true when the command should end.
   @Override
