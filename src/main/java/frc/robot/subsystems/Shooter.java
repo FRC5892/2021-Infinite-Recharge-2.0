819033eebbc305496c9d858060e3c01a800c933f
Micah Guttman <memtech3@gmail.com>
2021-02-13 15:00:01 -0600

    using the proper PID controller

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,39 +4,32 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Shooter extends PIDSubsystem {
+public class Shooter extends SubsystemBase {
   CANSparkMax shooterMotor1;
   CANSparkMax shooterMotor2;
-  CANEncoder shooterMotor1Encoder;
+  CANPIDController shooterPIDController;
   /** Creates a new Shooter. */
   public Shooter() {
-    super(
-      // The PIDController used by the subsystem
-      new PIDController(1, 1, 1));
-      setSetpoint(Constants.Shooter.SHOOTER_TARGET_SPEED);
-      shooterMotor1 = new CANSparkMax(Constants.Shooter.SHOOTER_MOTOR_1_ID, MotorType.kBrushless);
-      shooterMotor1Encoder = shooterMotor1.getEncoder();
-      shooterMotor2 = new CANSparkMax(Constants.Shooter.SHOOTER_MOTOR_2_ID, MotorType.kBrushless);
-      shooterMotor2.follow(shooterMotor1, true);
-    }
+    shooterMotor1 = new CANSparkMax(Constants.Shooter.SHOOTER_MOTOR_1_ID, MotorType.kBrushless);
+    shooterMotor2 = new CANSparkMax(Constants.Shooter.SHOOTER_MOTOR_2_ID, MotorType.kBrushless);
+    shooterMotor2.follow(shooterMotor1, true);
+    shooterPIDController = shooterMotor1.getPIDController();
+    shooterPIDController.setP(1);
+    shooterPIDController.setI(1);
+    shooterPIDController.setD(1);
+    shooterPIDController.setFF(1);
 
-  @Override
-  public void useOutput(double output, double setpoint) {
-    // Use the output here
-    shooterMotor1.pidWrite(output);
   }
 
   @Override
-  public double getMeasurement() {
-    // Return the process variable measurement here
-    return shooterMotor1Encoder.getVelocity();
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
