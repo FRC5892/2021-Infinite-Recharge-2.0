e75f56da31c718efa6f9fad061c5360ea1d0a36f
Micah Guttman <memtech3@gmail.com>
2021-02-20 00:03:08 -0600

    simplified advancekicker end conditions
    Co-Authored-By: Kai Page <kai@quantaly.net>

--- src/main/java/frc/robot/commands/AdvanceKicker.java
+++ src/main/java/frc/robot/commands/AdvanceKicker.java
@@ -6,15 +6,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Kicker;
 
 public class AdvanceKicker extends CommandBase {
   private final Kicker kicker;
   Timer timer;
-  private boolean finish;
   /** Creates a new AdvanceKicker. */
   public AdvanceKicker(Kicker k) {
-    finish = false;
     kicker = k;
     addRequirements(kicker);
     timer = new Timer();
@@ -24,24 +23,20 @@ public class AdvanceKicker extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    finish = false;
     if (kicker.ballLoaded() != true ){
       timer.reset();
       timer.start();
-      kicker.startKicker();
+      kicker.setKicker(Constants.Kicker.KICKER_MOTOR_ADVANCE_SPEED);
     }
   }
   
+  //advance slowly when called unless shooting
+  //if shooting and shooter is at setpoint advance at full speed
+  //if ball present at shoot setpoint and shooting nudge kicker
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if (timer.get() > 2) {
-      finish = true;
-    }
-    if (kicker.ballLoaded()) {
-      finish = true;
-    }
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
@@ -52,6 +47,6 @@ public class AdvanceKicker extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return finish;
+    return kicker.ballLoaded() || timer.get() > 2;
   }
 }
