5e4280f840711af08b8a992e2e0877c27b69fbe5
Micah Guttman <memtech3@gmail.com>
2021-02-22 00:03:02 -0600

    reformatted constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,64 +14,62 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    //drive train motor CAN addresses
-    public final class DriveTrain {
-        public static final int LEFT_MOTOR1_ID = 1;
-        public static final int LEFT_MOTOR2_ID = 2;
-        public static final int LEFT_MOTOR3_ID = 3;
-        public static final int RIGHT_MOTOR1_ID = 4;
-        public static final int RIGHT_MOTOR2_ID = 5;
-        public static final int RIGHT_MOTOR3_ID = 6;
-        public static final double SPEED = 1;
-		    public static final double SLOW_SPEED = .85;
-        public static final double AUTONOMOUS_SPEED = 1; 
-        public static final double DRIVE_FORWARD_TIME = 3;
-    }
-    public final class XboxController {
-        public static final int LEFT_Y_AXIS = 0;
-        public static final int LEFT_X_AXIS = 1;
-        public static final int JOYSTICK_NUMBER = 0;
-        public static final int ROLLER_AXIS = 3;
-    }
+  //drive train motor CAN addresses
+  public final class DriveTrain {
+    public static final int LEFT_MOTOR1_ID = 1;
+    public static final int LEFT_MOTOR2_ID = 2;
+    public static final int LEFT_MOTOR3_ID = 3;
+    public static final int RIGHT_MOTOR1_ID = 4;
+    public static final int RIGHT_MOTOR2_ID = 5;
+    public static final int RIGHT_MOTOR3_ID = 6;
+    public static final double SPEED = 1;
+    public static final double SLOW_SPEED = .85;
+    public static final double AUTONOMOUS_SPEED = 1; 
+    public static final double DRIVE_FORWARD_TIME = 3;
+  }
+  public final class XboxController {
+    public static final int LEFT_Y_AXIS = 0;
+    public static final int LEFT_X_AXIS = 1;
+    public static final int JOYSTICK_NUMBER = 0;
+    public static final int ROLLER_AXIS = 3;
+  }
 
-    public final class Intake {
-        public static final double EXTEND_ROLLER_SPEED = .25;
-        public static final int SOLENOID_FORWARD = 2;
-        public static final int SOLENOID_REVERSE = 5;
-		public static final double DISLODGE_SPIN_EXTEND_TIME =.5;
-		public static final double DISLODGE_ROLLERS_SPEED = 0.5;
-		public static final double ROLLER_SPEED = 1;
-		public static final int ROLLER_MOTOR_PORT = 3;
-		public static final double DISLODGE_SPIN_REVERSE_TIME = .5;
-		public static final double DISLODGE_SPIN_RETRACT_TIME = 1;
-    }
-    public final class Accumulator {
-		public static final int ACCUMULATOR_MOTOR_PORT = 4;
-		public static final double ACCUMULATOR_MOTOR_SPEED = 1;
-    }
-    public final class Kicker {
-		public static final int KICKER_MOTOR_PORT = 5;
-		public static final double KICKER_MOTOR_ADVANCE_SPEED = .75;
-		public static final int KICKER_SENSOR_PORT = 3;
-		public static final double KICKER_MOTOR_NUDGE_SPEED = 1;
-    }
-    public final class Shooter {
-        public static final int SHOOTER_MOTOR_1_ID = 7;
-		public static final int SHOOTER_MOTOR_2_ID = 8;
-		public static final double SHOOTER_TARGET_SPEED = 4000;
-		public static final double SHOOTER_SHOT_DIFFERENCE = 100;
-		public class ShooterPID {
-
-            public static final double P = 0.00105;
-            public static final double I = 0;
-            public static final double D = 0.06;
-            public static final double FF = 0.00018;
-		}
-    }
+  public final class Intake {
+    public static final double EXTEND_ROLLER_SPEED = .25;
+    public static final int SOLENOID_FORWARD = 2;
+    public static final int SOLENOID_REVERSE = 5;
+    public static final double DISLODGE_SPIN_EXTEND_TIME =.5;
+    public static final double DISLODGE_ROLLERS_SPEED = 0.5;
+    public static final double ROLLER_SPEED = 1;
+    public static final int ROLLER_MOTOR_PORT = 3;
+    public static final double DISLODGE_SPIN_REVERSE_TIME = .5;
+    public static final double DISLODGE_SPIN_RETRACT_TIME = 1;
+  }
+  public final class Accumulator {
+    public static final int ACCUMULATOR_MOTOR_PORT = 4;
+    public static final double ACCUMULATOR_MOTOR_SPEED = 1;
+  }
+  public final class Kicker {
+    public static final int KICKER_MOTOR_PORT = 5;
+    public static final double KICKER_MOTOR_ADVANCE_SPEED = .75;
+    public static final int KICKER_SENSOR_PORT = 3;
+    public static final double KICKER_MOTOR_NUDGE_SPEED = 1;
+  }
+  public final class Shooter {
+    public static final int SHOOTER_MOTOR_1_ID = 7;
+    public static final int SHOOTER_MOTOR_2_ID = 8;
+    public static final double SHOOTER_TARGET_SPEED = 4000;
+    public static final double SHOOTER_SHOT_DIFFERENCE = 100;
+      public class ShooterPID {
+        public static final double P = 0.00105;
+        public static final double I = 0;
+        public static final double D = 0.06;
+        public static final double FF = 0.00018;
+      }
+  }
   public class Hood {
-
-	public static final int HOOD_MOTOR_PORT = 6;
-	public static final int HOOD_BOTTOM_STOP = 1;
-	public static final int HOOD_TOP_STOP = 5;
-	public static final int HOOD_POTENTIOMETER = 1;}
+    public static final int HOOD_MOTOR_PORT = 6;
+    public static final int HOOD_BOTTOM_STOP = 1;
+    public static final int HOOD_TOP_STOP = 5;
+    public static final int HOOD_POTENTIOMETER = 1;}
 }
