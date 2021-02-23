154e32145f779771ccc0d24661da0821f90a1a7d
Micah Guttman <memtech3@gmail.com>
2021-02-22 23:47:39 -0600

    changed drive slow button so that it wouldn't conflict with sethood

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -25,7 +25,7 @@ public class DriveWithJoysticks extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (RobotContainer.driverJoystick.getXButton()) {
+    if (RobotContainer.driverJoystick.getYButton()) {
       driveTrain.driveWithJoysticks(RobotContainer.driverJoystick, Constants.DriveTrain.SLOW_SPEED, Constants.DriveTrain.SLOW_SPEED);
     }
     else {
