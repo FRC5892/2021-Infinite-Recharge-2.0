e6b376a880dd318fac3aa5788b448cfb30667dcd
Micah Guttman <memtech3@gmail.com>
2021-02-22 22:35:46 -0600

    commented out setSetpoint for diagnostics

--- src/main/java/frc/robot/commands/ShootBall.java
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -26,7 +26,7 @@ public class ShootBall extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooter.setSetpoint(Constants.Shooter.SHOOTER_TARGET_SPEED);
+    //shooter.setSetpoint(Constants.Shooter.SHOOTER_TARGET_SPEED);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
