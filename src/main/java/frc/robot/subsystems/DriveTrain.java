3204662649cb8550eb1053238171246de1bd92b0
Micah Guttman <memtech3@gmail.com>
2021-02-26 16:07:53 -0600

    trajectories expiramentation

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,13 +4,19 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.MathUtils;
@@ -22,9 +28,13 @@ public class DriveTrain extends SubsystemBase {
   CANSparkMax rightMotor1;
   CANSparkMax rightMotor2;
   CANSparkMax rightMotor3;
+  CANEncoder leftEncoder;
+  CANEncoder rightEncoder;
   SpeedControllerGroup leftMotors;
   SpeedControllerGroup rightMotors;
   DifferentialDrive drive;
+  AHRS gyro;
+  DifferentialDriveOdometry odometry;
   /** Creates a new DriveTrain. */
   public DriveTrain() {
     leftMotor1 = new CANSparkMax(Constants.DriveTrain.LEFT_MOTOR1_ID, MotorType.kBrushless);
@@ -40,11 +50,18 @@ public class DriveTrain extends SubsystemBase {
     rightMotor3 = new CANSparkMax(Constants.DriveTrain.RIGHT_MOTOR3_ID, MotorType.kBrushless);
     rightMotor3.setInverted(false);
 
+    leftEncoder = leftMotor1.getEncoder();
+    rightEncoder = rightMotor1.getEncoder();
+    resetEncoders();
+
     leftMotors = new SpeedControllerGroup(leftMotor1, leftMotor2, leftMotor3);
     rightMotors = new SpeedControllerGroup(rightMotor1, rightMotor2, rightMotor3);
     drive = new DifferentialDrive(leftMotors, rightMotors);
-  }
 
+    gyro = new AHRS(SPI.Port.kMXP);
+    odometry = new DifferentialDriveOdometry(gyro.getRotation2d());
+  }
+  
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Left Motor 1", leftMotor1.get());
@@ -55,9 +72,10 @@ public class DriveTrain extends SubsystemBase {
     SmartDashboard.putNumber("Right Motor 2", rightMotor2.get());
     SmartDashboard.putNumber("Right Motor 3", rightMotor3.get());
     SmartDashboard.putNumber("Right Motors", rightMotors.get());
+    odometry.update(gyro.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
     // This method will be called once per scheduler run
   }
-
+  
   public void driveWithJoysticks(XboxController controller, double xSpeed, double zRotation) {
     drive.arcadeDrive(MathUtils.signedSquare(controller.getRawAxis(Constants.XboxController.LEFT_X_AXIS))*xSpeed, MathUtils.signedSquare(-controller.getRawAxis(4))*zRotation, false);
     //squares the controller input before the speed factor is multiplied to make the drive smoother
@@ -71,6 +89,65 @@ public class DriveTrain extends SubsystemBase {
 
   public void arcadeDrive(double xSpeed, double zRotation) {
     drive.arcadeDrive(xSpeed, zRotation);
+  }
+
+  public void resetEncoders() {
+    leftEncoder.setPosition(0);
+    leftEncoder.setPositionConversionFactor(1);
+    leftEncoder.setVelocityConversionFactor(1);
+    rightEncoder.setPosition(0);
+    rightEncoder.setPositionConversionFactor(1);
+    rightEncoder.setVelocityConversionFactor(1);
+  }
+
+  public void setMaxOutput(double maxOutput) {
+    drive.setMaxOutput(maxOutput);
+  }
+
+  //Used in trajectory
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    odometry.resetPosition(pose, gyro.getRotation2d());
+  }
+  
+  public void zeroHeading() {
+    gyro.reset();
+  }
+
+  public double getAverageEncoderDistance() {
+    return (leftEncoder.getPosition() + rightEncoder.getPosition())/2;
+  }
+
+  public CANEncoder getLeftEncoder() {
+    return leftEncoder;
+  }
+
+  public CANEncoder getRightEncoder() {
+    return rightEncoder;
+  }
+
+  public double getHeading() {
+    return gyro.getRotation2d().getDegrees();
+  }
+
+  //Used in trajectory
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+  //Used in trajectory
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftEncoder.getVelocity(), rightEncoder.getVelocity());
+  }
+
+  public double getTurnRate() {
+    return -gyro.getRate();
+  }
+
+  public void tankDriveVolts(double lefttVolts, double rightVolts) {
+    leftMotors.setVoltage(lefttVolts);
+    rightMotors.setVoltage(rightVolts);
+    drive.feed();
   }
 
   public void stop(){
