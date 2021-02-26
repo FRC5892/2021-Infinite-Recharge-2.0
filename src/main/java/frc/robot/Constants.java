92d268bd76c5eda731d559bb80ea29aac6d01093
Micah Guttman <memtech3@gmail.com>
2021-02-26 16:07:53 -0600

    trajectories expiramentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,6 +26,20 @@ public final class Constants {
     public static final double SLOW_SPEED = .85;
     public static final double AUTONOMOUS_SPEED = 1; 
     public static final double DRIVE_FORWARD_TIME = 3;
+    public class DriveCharacteristics {
+      //characteristics from robot characterization tool
+      public static final double VOLTS = 0;
+      public static final double VOLT_SECONDS_PER_METER = 0;
+      public static final double VOLT_SECONDS_SQUARED_PER_METER = 0;
+      //width between sets of wheels
+      public static final double TRACK_WIDTH = 0;
+      //max speed and acceleration
+      public static final double MAX_SPEED = 0;
+      public static final double MAX_ACCELERATION = 0;
+      //RAMSETE Values
+      public static final double RAMSETE_B = 0;
+      public static final double RAMSETE_ZETA = 0;
+    }
   }
   public final class XboxController {
     public static final int LEFT_Y_AXIS = 0;
