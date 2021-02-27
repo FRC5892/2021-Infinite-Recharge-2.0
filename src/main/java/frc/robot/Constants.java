efbfcd4183d02df17aa9f9de461378c367b563dd
Micah Guttman <memtech3@gmail.com>
2021-02-26 23:21:32 -0600

    fixed bracket error

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -71,10 +71,11 @@ public final class Constants {
     public static final int HOOD_MOTOR_PORT = 6;
     public static final int HOOD_BOTTOM_STOP = 1;
     public static final int HOOD_TOP_STOP = 5;
-    public static final int HOOD_POTENTIOMETER = 1;}
-}
+    public static final int HOOD_POTENTIOMETER = 1;
+  }
 public final class Limelight {
   private static final double LIMELIGHT_MOUNTING_ANGLE = 30; // degrees
   private static final double LIMELIGHT_MOUNTING_HEIGHT = 18; // inches
   private static final double LIMELIGHT_TARGET_HEIGHT = 98.25; // inches
+}
 }
