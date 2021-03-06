ed324d0533ff78c1148304597681ce3a306a1754
Micah Guttman <memtech3@gmail.com>
2021-03-05 23:18:01 -0600

    Sethood from networktables, hood pot to angle (#16)
    * sethood from networktables
    
    * fixed wrong networktable key
    
    * hood pot now works with angles instead of raw voltage

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.VictorSP;
@@ -11,45 +13,51 @@ import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants;
+import frc.robot.commands.SetHood;
 
 public class Hood extends PIDSubsystem {
   VictorSP hoodMotor;
   DigitalInput bottomStop;
   DigitalInput topStop;
   AnalogInput hoodPotentiometer;
+  NetworkTableInstance networkTableInstance;
+  NetworkTable smartdashboardTable;
   /** Creates a new Hood. */
   public Hood() {
     super(
       // The PIDController used by the subsystem
-      new PIDController(2, 0, 0));
+      new PIDController(0.5, 0, 0));
       getController().setTolerance(1);
       hoodMotor = new VictorSP(Constants.Hood.HOOD_MOTOR_PORT);
       bottomStop = new DigitalInput(Constants.Hood.HOOD_BOTTOM_STOP);
       topStop = new DigitalInput(Constants.Hood.HOOD_TOP_STOP);
       hoodPotentiometer = new AnalogInput(Constants.Hood.HOOD_POTENTIOMETER);
-  }
-
-  public void setHoodVictorSPVictorSPPosition(double setpoint) {
-    this.setSetpoint(setpoint);
-  }
-  
-  public boolean atDirectionStop() {
-    return (hoodMotor.getSpeed()>0 && topStop.get())||(hoodMotor.getSpeed()<0 && !bottomStop.get());
-  }
-
-  public boolean atSetpoint() {
-    return hoodPotentiometer.getAverageVoltage() == this.getSetpoint();
-  }
-  @Override
-  public void useOutput(double output, double setpoint) {
-    hoodMotor.set(output);
-    // Use the output here
-  }
-
-  @Override
-  public double getMeasurement() {
+    }
+    public double getHoodAngle () {
+      return hoodPotentiometer.getAverageVoltage()*25.23+27.37;
+    }
+    public void setHood(double setpoint) {
+      this.setSetpoint(setpoint);
+    }
+    
+    public boolean atDirectionStop() {
+      return (hoodMotor.getSpeed()>0 && topStop.get())||(hoodMotor.getSpeed()<0 && !bottomStop.get());
+    }
+    
+    public boolean atSetpoint() {
+      return hoodPotentiometer.getAverageVoltage() == this.getSetpoint();
+    }
+    @Override
+    public void useOutput(double output, double setpoint) {
+      hoodMotor.set(output);
+      // Use the output here
+    }
+    
+    @Override
+    public double getMeasurement() {
     SmartDashboard.putNumber("Hood Setpoint", this.getSetpoint());
-    SmartDashboard.putNumber("Hood Potentiometer", hoodPotentiometer.getAverageVoltage());
-    return hoodPotentiometer.getAverageVoltage();
+    SmartDashboard.setDefaultNumber("Set Hood Angle", 0);
+    SmartDashboard.putNumber("Hood Potentiometer", getHoodAngle());
+    return getHoodAngle();
   }
 }
