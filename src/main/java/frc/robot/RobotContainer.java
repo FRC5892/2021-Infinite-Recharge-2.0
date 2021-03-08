1a09af18de8749cb5d65a05ff5d481210157b603
Micah Guttman <memtech3@gmail.com>
2021-03-08 10:18:22 -0600

    shooter now shoots with error of +-100 rpm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.AdvanceKicker;
 import frc.robot.commands.DriveForwardTimed;
 import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.commands.ExampleCommand;
@@ -58,7 +57,6 @@ public class RobotContainer {
 
   //declaring kicker and kicker commands
   private final Kicker kicker;
-  private final AdvanceKicker advanceKicker;
   private final RunKicker runKicker;
 
   //declaring shooter
@@ -98,7 +96,6 @@ public class RobotContainer {
 
 
     kicker = new Kicker();
-    advanceKicker = new AdvanceKicker(kicker);
     runKicker = new RunKicker(kicker);
     kicker.setDefaultCommand(runKicker);
 
@@ -125,8 +122,6 @@ public class RobotContainer {
   private void configureButtonBindings() {
     JoystickButton intakeToggleButton = new JoystickButton(driverJoystick, XboxController.Button.kA.value);
     intakeToggleButton.whenPressed(intakeToggle);
-    JoystickButton kickerAdvanceButton = new JoystickButton(driverJoystick, XboxController.Button.kB.value);
-    kickerAdvanceButton.whenPressed(advanceKicker);
     JoystickButton spinShooterButton = new JoystickButton(driverJoystick, XboxController.Button.kStart.value);
     spinShooterButton.whileHeld(shootBall);
     JoystickButton setHoodButton = new JoystickButton(driverJoystick, XboxController.Button.kX.value);
