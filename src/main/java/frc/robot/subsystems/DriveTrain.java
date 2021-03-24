b1477adb1e12b3dc730b7575f4e7fa0f42bfcc0e
Micah Guttman <memtech3@gmail.com>
2021-03-24 17:50:25 -0500

    made function to create motors, more DRY

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -24,20 +25,21 @@ public class DriveTrain extends SubsystemBase {
   SpeedControllerGroup leftMotors;
   SpeedControllerGroup rightMotors;
   DifferentialDrive drive;
+  public CANSparkMax driveCANSparkMax (int ID) {
+    CANSparkMax sparkMax = new CANSparkMax(ID, MotorType.kBrushless);
+    sparkMax.restoreFactoryDefaults();
+    sparkMax.setInverted(false);
+    sparkMax.setIdleMode(IdleMode.kBrake);
+    return sparkMax;
+  }
   /** Creates a new DriveTrain. */
   public DriveTrain() {
-    leftMotor1 = new CANSparkMax(Constants.DriveTrain.LEFT_MOTOR1_ID, MotorType.kBrushless);
-    leftMotor1.setInverted(false);
-    leftMotor2 = new CANSparkMax(Constants.DriveTrain.LEFT_MOTOR2_ID, MotorType.kBrushless);
-    leftMotor2.setInverted(false);
-    leftMotor3 = new CANSparkMax(Constants.DriveTrain.LEFT_MOTOR3_ID, MotorType.kBrushless);
-    leftMotor3.setInverted(false);
-    rightMotor1 = new CANSparkMax(Constants.DriveTrain.RIGHT_MOTOR1_ID, MotorType.kBrushless);
-    rightMotor1.setInverted(false);
-    rightMotor2 = new CANSparkMax(Constants.DriveTrain.RIGHT_MOTOR2_ID, MotorType.kBrushless);
-    rightMotor2.setInverted(false);
-    rightMotor3 = new CANSparkMax(Constants.DriveTrain.RIGHT_MOTOR3_ID, MotorType.kBrushless);
-    rightMotor3.setInverted(false);
+    leftMotor1 = driveCANSparkMax(Constants.DriveTrain.LEFT_MOTOR1_ID);
+    leftMotor2 = driveCANSparkMax(Constants.DriveTrain.LEFT_MOTOR2_ID);
+    leftMotor3 = driveCANSparkMax(Constants.DriveTrain.LEFT_MOTOR3_ID);
+    rightMotor1 = driveCANSparkMax(Constants.DriveTrain.RIGHT_MOTOR1_ID);
+    rightMotor2 = driveCANSparkMax(Constants.DriveTrain.RIGHT_MOTOR2_ID);
+    rightMotor3 = driveCANSparkMax(Constants.DriveTrain.RIGHT_MOTOR3_ID);
 
     leftMotors = new SpeedControllerGroup(leftMotor1, leftMotor2, leftMotor3);
     rightMotors = new SpeedControllerGroup(rightMotor1, rightMotor2, rightMotor3);
