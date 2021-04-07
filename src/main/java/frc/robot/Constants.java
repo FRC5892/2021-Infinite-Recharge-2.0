ef4e5a4811047d5fb0ef89d64d2574bee5335aa8
Micah Guttman <memtech3@gmail.com>
2021-04-07 16:10:24 -0500

    added appropriate characterization values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,11 +30,11 @@ public final class Constants {
       //distance unit is meters
       //characteristics from robot characterization tool
       //kV in characterization tool
-      public static final double VOLTS = 12.8;
+      public static final double VOLTS = 1.76;
       //kS in characterization tool
-      public static final double VOLT_SECONDS_PER_METER = 0.237;
+      public static final double VOLT_SECONDS_PER_METER = 0.245;
       //kA in characterization tool
-      public static final double VOLT_SECONDS_SQUARED_PER_METER = 3.1;
+      public static final double VOLT_SECONDS_SQUARED_PER_METER = 0.425;
       //width between sets of wheels
       public static final double TRACK_WIDTH = 0.572516;
       //max speed and acceleration
