3895e9544af4b89dca8ac4587f576e23db8d0825
Micah Guttman <memtech3@gmail.com>
2021-05-29 16:52:05 -0500

    Faster kicker

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
 
 	public final class Kicker {
 		public static final int KICKER_MOTOR_PORT = 5;
-		public static final double KICKER_MOTOR_ADVANCE_SPEED = .25;
+		public static final double KICKER_MOTOR_ADVANCE_SPEED = .40;
 		public static final int KICKER_SENSOR_PORT = 3;
 		public static final double KICKER_MOTOR_NUDGE_SPEED = 1;
 		public static final double KICKER_TIME_OUT = 2;
